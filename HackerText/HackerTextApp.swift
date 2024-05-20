c17b9d700d648f74bc7bfb2273850882c6a05117
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-05-20 10:04:59 -0500

    Initial Commit

---
+++ HackerText/HackerTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerTextApp.swift
+//  HackerText
+//
+//  Created by Justin Hold on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
