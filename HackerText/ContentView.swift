f0593bdc031858d2d63744db43499e7dc3adc355
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-05-21 11:06:09 -0500

    Hacker Text Bro

--- HackerText/ContentView.swift
+++ HackerText/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+	
+	// MARK: - Properties
+	@State private var trigger: Bool = false
+	@State private var isClicked: Bool = false
+	@State private var text: String = "Hello, World!"
+	
+	// MARK: - View Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+		VStack(alignment: .leading, spacing: 12) {
+			HackerTextView(
+				text: text,
+				trigger: trigger,
+				transition: .interpolate,
+				speed: 0.06
+			)
+				.font(.largeTitle.bold())
+				.lineLimit(4)
+			
+			Button(action: {
+				if text == "Hello, World!" {
+					text = "This is Hacker\nText View."
+				} else if text == "This is Hacker\nText View." {
+					text = "Made with <3\nin TX by @leftHandedApps"
+				} else {
+					text = "Hello, World!"
+				}
+				trigger.toggle()
+			}, label: {
+				Text("Trigger")
+					.fontWeight(.semibold)
+					.padding(.horizontal, 15)
+					.padding(.vertical, 2)
+			})
+			.buttonStyle(.borderedProminent)
+			.buttonBorderShape(.capsule)
+			.frame(maxWidth: .infinity)
+			.padding(.top, 30)
+			.opacity(isClicked ? 0 : 1)
         }
-        .padding()
+		.padding(15)
+		.frame(maxWidth: .infinity, alignment: .leading)
+        
+		
     }
 }
 
