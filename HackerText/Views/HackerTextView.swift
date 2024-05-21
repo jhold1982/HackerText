eee5ea733d73ed50ce32a79d86931c9bcfb3d8bb
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-05-21 11:05:52 -0500

    first commit

---
+++ HackerText/Views/HackerTextView.swift
@@ -0,0 +1,18 @@
+//
+//  HackerTextView.swift
+//  HackerText
+//
+//  Created by Justin Hold on 5/20/24.
+//
+
+import SwiftUI
+
+struct HackerTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HackerTextView()
+}
